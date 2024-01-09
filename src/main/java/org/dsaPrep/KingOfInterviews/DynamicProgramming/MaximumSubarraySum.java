2eb0ff6af51edcbb08b9177bb63e540cfd25d33a
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2024-01-10 00:03:26 +0530

    MaximumSubArraySum

--- src/main/java/org/dsaPrep/KingOfInterviews/DynamicProgramming/MaximumSubarraySum.java
+++ src/main/java/org/dsaPrep/KingOfInterviews/DynamicProgramming/MaximumSubarraySum.java
@@ -1,11 +1,8 @@
 package org.dsaPrep.KingOfInterviews.DynamicProgramming;
-//3rd Jan, 2024
+//9th Jan, 2024
 
 //*******************//
-//Coin Change: You are given an integer array coins representing coins of different denominations
-// and an integer amount representing a total amount of money. Return the fewest number of coins
-// that you need to make up that amount. If that amount of money cannot be made up by any combination
-// of the coins, return -1. You may assume that you have an infinite number of each kind of coin.
+//Maximum Subarray Sum: Given an integer array nums, find the subarray with the largest sum, and return its sum.
 //*******************//
 
 public class MaximumSubarraySum {
